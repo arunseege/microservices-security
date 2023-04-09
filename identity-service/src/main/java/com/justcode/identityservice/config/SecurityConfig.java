d7e204e1aec9dce7b3367227391a7b4c02a2fc76
arunseege <arunsphassan@gmail.com>
2023-04-09 19:23:15 +0530

    implemented jwt,created filter ,jwtservice, and modified securityconfig

--- identity-service/src/main/java/com/justcode/identityservice/config/SecurityConfig.java
+++ identity-service/src/main/java/com/justcode/identityservice/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.justcode.identityservice.config;
 
+import com.justcode.identityservice.filter.JwtAuthFilter;
 import com.justcode.identityservice.service.UserInfoUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,11 +20,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    @Autowired
+    private JwtAuthFilter authFilter;
 
     @Bean
     public UserDetailsService userDetailsService(){
@@ -46,15 +54,21 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
      return   http.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/products/welcome","/products/new").permitAll()
+                .requestMatchers("/products/welcome","/products/new","/products/authenticate").permitAll()
                 .and()
                 .authorizeHttpRequests()
                 .requestMatchers("/products/**")
                 .authenticated()
                 .and()
-                .formLogin()
+             .sessionManagement()
+             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+             .and()
+             .authenticationProvider(authenticationProvider())
+             .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+             .build();
+                /*.formLogin()
                 .and()
-                .build();
+                .build();*/
     }
 
     @Bean
@@ -64,4 +78,9 @@ public class SecurityConfig {
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
     }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
 }
--- identity-service/src/main/java/com/justcode/identityservice/controller/ProductController.java
+++ identity-service/src/main/java/com/justcode/identityservice/controller/ProductController.java
@@ -1,10 +1,16 @@
 package com.justcode.identityservice.controller;
 
+import com.justcode.identityservice.dto.AuthRequest;
 import com.justcode.identityservice.dto.Product;
 import com.justcode.identityservice.entity.UserInfo;
+import com.justcode.identityservice.service.JwtService;
 import com.justcode.identityservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +21,12 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
 
     @GetMapping("/welcome")
@@ -37,6 +49,22 @@ public class ProductController {
     @PostMapping("/new")
     public String addNewUser(@RequestBody UserInfo userInfo){
         return productService.addUser(userInfo);
+    }
+
+    @PostMapping("/authenticate")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+       //auth manager lets only for users in db .to get token
+        //only when authentication successful then authorization
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
+
+       // return jwtService.generateToken(authRequest.getUsername());
+
+
     }
 
 }
